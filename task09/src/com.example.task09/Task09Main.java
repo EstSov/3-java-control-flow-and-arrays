0591ccc497b83d6640e169cd34f2a0c281a4c443
EstSov <fedorovama@gmail.com>
2021-09-25 15:15:24 +0500

    task09

--- task09/src/com.example.task09/Task09Main.java
+++ task09/src/com.example.task09/Task09Main.java
@@ -1,5 +1,8 @@
 package com.example.task09;
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 public class Task09Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -14,8 +17,16 @@ public class Task09Main {
     }
 
     static int min(int[] arr) {
+        int min = Integer.MAX_VALUE;
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(min>arr[i])
+            {
+                min = arr[i];
+            }
+        }
+        return min;
     }
 
 }
